3d222785b842008b668ef03212c8e4e324223b63
Ubuntu <ubuntu@ip-172-31-27-81.ap-northeast-1.compute.internal>
2015-09-13 03:01:36 +0000

    modify initData to accept argumentwq

--- TARS/CASE/initData.py
+++ TARS/CASE/initData.py
@@ -1,13 +1,14 @@
 import json
 import codecs
 import random
+import sys
 
 def initUser():
 	f = codecs.open('../data/user.json', 'w', encoding='utf8')
 	jsonData = { 'USERS': []}
-	for i in range(1, 10):
+	for i in range(1, int(sys.argv[1])+1):
 		temp = {}
-		for j in range(1, 5):
+		for j in range(1, int(sys.argv[2])+1):
 			temp[str(j)] = random.randint(0, 3) #0 = None, 1 = negative, 2 = neutral, 3 = positive
 
 		jsonData['USERS'].append({ 
